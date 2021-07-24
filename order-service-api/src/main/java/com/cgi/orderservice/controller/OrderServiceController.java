3f75b62adc0bfc4cf5c129477b227080f3c32e25
yogeshabc3 <87833296+yogeshabc3@users.noreply.github.com>
2021-07-24 18:36:05 +0100

    Refactoring and Exception Handling

--- order-service-api/src/main/java/com/cgi/orderservice/controller/OrderServiceController.java
+++ order-service-api/src/main/java/com/cgi/orderservice/controller/OrderServiceController.java
@@ -2,7 +2,9 @@ package com.cgi.orderservice.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,19 +16,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cgi.orderservice.domain.OrderDetail;
+import com.cgi.orderservice.domain.OrderSummaryList;
+import com.cgi.orderservice.service.OrderService;
+import com.cgi.orderservice.utils.OrderType;
 
 @RestController
 @RequestMapping("/api")
 public class OrderServiceController {
 	
 	
+	@Autowired
+	private OrderService service;
+	
 	@PostMapping("/orders")
-	public ResponseEntity<OrderDetail> createOrder(@RequestBody OrderDetail orderData) {
-		return new ResponseEntity<>(new OrderDetail(), HttpStatus.CREATED);		
+	public ResponseEntity<OrderSummaryList> createOrder(@RequestBody OrderDetail orderData) {
+		
+		OrderSummaryList orderSummaryList = service.findAllLiveOrders();
+		
+		return new ResponseEntity<>(orderSummaryList, HttpStatus.CREATED);		
 	}
 	
 	@PutMapping("/orders/cancel/{orderId}")
 	public ResponseEntity<OrderDetail> cancelOrder(@PathVariable String orderId) {
+		 Optional<OrderDetail> orderDetail = service.findByOrderId(orderId);
+		 if (orderDetail.isPresent()) {
+	         OrderDetail detail = orderDetail.get();
+	         detail.setOrderType(OrderType.CANCEL.name());
+	         return new ResponseEntity<>(service.save(detail), HttpStatus.OK);
+		 } else {
+			 throw new RecordNotFoundException("Order Details not found " + orderId);
+		 }
+	         
 		 return new ResponseEntity<>(new OrderDetail(), HttpStatus.OK);
 		
 	}
